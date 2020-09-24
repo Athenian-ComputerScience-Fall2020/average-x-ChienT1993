806b6050e407cc05fe8d97d5d4b243cdb1a9878e
ChienT1993 <21ctran@athenian.org>
2020-09-24 14:41:52 +0000

    finished

--- my_code.py
+++ my_code.py
@@ -1,16 +1,24 @@
-# Collaborators (including web sites where you got help: (enter none if you didn't need help)
+# Collaborators (including web sites where you got help: https://www.geeksforgeeks.org/python-get-a-list-as-input-from-user/
 #
 
 def avg(user_list):
-    # Insert code here
+    length = len(user_list)
+    total = sum(user_list)
+    average = total/length
     return average
 
 
 if __name__ == '__main__':
     # test your fucntion with this print statement before writing the input loop
-    print(avg([x, y, z]))    # Put the values you want to test in for x,y and z
+    #print(avg([3, 4, 5]))    # Put the values you want to test in for x,y and z
 
     # Now, comment out the print statement and work on the code below
     # Remember to indent in this section
-    user_list = [] # start with an empty list
+ try:
+    user_list = []
+
+    while True:
+        user_list.append(int(input("Enter a number (enter a letter to stop): ")))
+ except:
+    print (avg(user_list))
     # Write a loop to allow the user to input the values to be averaged
